43020217c3efcec33272064f17775e70df455963
jeferbc <jeffe.bernal@gmail.com>
2017-02-20 12:17:18 -0500

    h1 and p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <h1>Hola mundo</h1>
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p className="App-intro">
+          Esta es mi primera aplicación en React, está padrísimo!
+        </p>
+      </div>
     );
   }
 }
